f6d700da9e92b249a5c21307daadda5d6614e13f
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-15 14:00:58 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 //Task 4: Import components into an App component
 //Develop a functional component called  App that imports and displays your  Header,   About, and Contact components.
-import Header from "./components/Header";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import Header from "./h./Header";
+import About from "./h./About";
+import Contact from "./h./Contact";
 
 function App() {
 
